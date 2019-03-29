5017f9c610b656d7b6e2f42108bb90a131fd85c1
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-29 15:44:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ client.on('message', (message) => {
         case 'clear':
             if (!message.guild.me.voiceChannel) { return message.channel.send('⚠ I\'m not connected in any channel. Try `' + client.config.BOT_PREFIX + 'join` to join the bot.'); }
             if (queue.length === 0) { return message.channel.send('⚠ The queue is empty.'); }
-            queue.splice(1, queue.length);
+            if (queue.length !== 1) { queue.splice(1, queue.length); }
             message.channel.send('✅ The queue is delete.');
             break;
         case 'help':
